b04b88507d7df153b1698ec695f0d702cb189559
Max <linuxcitizen@gmail.com>
2020-12-19 14:06:42 +0300

    Testing part

---
+++ src/test/java/com/example/webapp/UserSaveTest.java
@@ -0,0 +1,32 @@
+package com.example.webapp;
+
+import com.example.webapp.dto.UserDto;
+import com.example.webapp.model.User;
+import com.example.webapp.service.impl.UserServiceImpl;
+import org.hamcrest.Matcher;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+@SpringBootTest
+public class UserSaveTest {
+
+    @Autowired
+    private UserServiceImpl userService;
+
+    @Test
+    public void saveUserTest() {
+        User user = userService.createUser(UserDto.builder()
+                .firstName("firstName")
+                .lastName("lastName")
+                .email("email@mail.com")
+                .build());
+        assertThat(user, is(notNullValue()));
+    }
+
+    private void assertThat(User user, Matcher<Object> objectMatcher) {
+    }
+}
